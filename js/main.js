75c4ca4b3f5f90c910dadfea8b0492912a20b656
maraja <amit.maraj@gmail.com>
2018-01-16 01:13:52 -0500

    a few changes. added now ui kit.

--- js/main.js
+++ js/main.js
@@ -240,6 +240,27 @@
 
 	};
 
+	var stickyNav = function() {
+		// $("nav").stick_in_parent();
+		// When the user scrolls the page, execute myFunction 
+		window.onscroll = function() {myFunction()};
+
+		// Get the header
+		var header = document.getElementById("navigation");
+
+		// Get the offset position of the navbar
+		var sticky = header.offsetTop;
+
+		// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+		function myFunction() {
+		  if (window.pageYOffset >= sticky) {
+		    header.classList.add("sticky-nav");
+		  } else {
+		    header.classList.remove("sticky-nav");
+		  }
+		}
+	}
+
 
 
 	$(function(){
@@ -252,6 +273,7 @@
 		goToTop();
 		loaderPage();
 		counterWayPoint();
+		stickyNav();
 	});
 
 
